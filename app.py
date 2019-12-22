fc258358952a31314c3d3f4e497d763ed333a0df
jxavie <54754878+jxavie@users.noreply.github.com>
2019-12-22 04:36:07 -0500

    uploading files

--- app.py
+++ app.py
@@ -1,24 +1,13 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
-import os
-
-# import credentials
-username = os.environ.get("username")
-password = os.environ.get("password")
-cluster = os.environ.get("cluster")
-# remote_db_port = os.environ.get("remote_db_port")
-dbname = os.environ.get("dbname")
-uri = os.environ.get('uri')
 
 # Create an instance of Flask
 app = Flask(__name__)
-# app.config["MONGO_URI"] = f"mongodb+srv://{username}:{password}@{cluster}/{dbname}?retryWrites=true&w=majority"
 
 # Use PyMongo to establish Mongo connection
-# mongo = PyMongo(app, uri='mongodb://localhost:27017/mars_scraped')
-# address = f'mongodb+srv://{username}:{password}@{cluster}/{dbname}?retryWrites=true&w=majority'
-mongo = PyMongo(app, uri=uri)
+mongo = PyMongo(app, uri='mongodb://localhost:27017/mars_scraped')
+
 
 # Route to render index.html template using data from Mongo
 @app.route('/')
--- scrape_mars.py
+++ scrape_mars.py
@@ -5,26 +5,14 @@ from splinter.exceptions import ElementDoesNotExist
 import time
 import pandas as pd
 
-from selenium import webdriver
 
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google-chrome'
-CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = GOOGLE_CHROME_PATH
-
-# def init_browser():
-#     # executable_path = {"executable_path": "chromedriver.exe"}
-#     # return Browser("chrome", **executable_path, headless=False)
-#     browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-#     return browser
+def init_browser():
+    executable_path = {"executable_path": "chromedriver.exe"}
+    return Browser("chrome", **executable_path, headless=False)
 
 
 def scrape():
-    # browser = init_browser()
-    browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    browser = init_browser()
 
     # visit NASA's Mars Exploration Program news site
     url_news = 'https://mars.nasa.gov/news'
